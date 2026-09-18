e14e5f2cad8f0af7ea2c2a686f0c1b77ae0d9169
AnnonyA <79977221+AnnonyA@users.noreply.github.com>
2026-09-18 16:38:32 -0300

    test: exercise every multiline fuzz input

--- internal/cli/config_bound_test.go
+++ internal/cli/config_bound_test.go
@@ -8,15 +8,13 @@ import (
 )
 
 func FuzzBoundedSafeMultilineDisplayText(f *testing.F) {
-	f.Add("plain text", 32)
-	f.Add("line one\nline two\tvalue", 64)
-	f.Add("\x1b[31mred\x1b[0m\u202Ehidden", 16)
-	f.Add(strings.Repeat("界", 128), 12)
+	f.Add("plain text", uint16(32))
+	f.Add("line one\nline two\tvalue", uint16(64))
+	f.Add("\x1b[31mred\x1b[0m\u202Ehidden", uint16(16))
+	f.Add(strings.Repeat("界", 128), uint16(12))
 
-	f.Fuzz(func(t *testing.T, input string, maxRunes int) {
-		if maxRunes < 0 || maxRunes > 4096 {
-			return
-		}
+	f.Fuzz(func(t *testing.T, input string, rawLimit uint16) {
+		maxRunes := int(rawLimit % 4097)
 		got := boundedSafeMultilineDisplayText(input, maxRunes)
 		if utf8.RuneCountInString(got) > maxRunes {
 			t.Fatalf("output has %d runes, limit is %d", utf8.RuneCountInString(got), maxRunes)
